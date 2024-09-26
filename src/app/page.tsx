ba1597304d01abf625307a0beb094da7c2dc1b64
rodrigo-barboza <rodrigoleandro.adm@gmail.com>
2024-09-25 22:46:18 -0300

    adiciona na pagina inicial os componentes criados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-import Image from "next/image";
+import InteractionRoom from "@/components/InteractionRoom";
+import Room from "@/components/Room";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <section className="flex justify-between gap-[100px]">
+      <InteractionRoom />
+      <Room />
+    </section>
   );
 }
