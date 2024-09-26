1d11c9901625a428ca9cffe7002fd02fa2a35f73
rodrigo-barboza <rodrigoleandro.adm@gmail.com>
2024-09-25 22:46:06 -0300

    adiciona componente da sala

---
+++ src/components/Room/index.tsx
@@ -0,0 +1,11 @@
+
+export default function Room() {
+    return (
+        <div className="w-1/2">
+            <div className="text-5xl font-extrabold text-[#232528]">Sala</div>
+            <div className="mt-8 rounded-[32px] bg-gray-200 p-6 min-h-[400px]">
+                Texto bacana
+            </div>
+        </div>
+    )
+};
