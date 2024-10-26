f7ef9e33d4a0b2e2b3b7fb6f5dc762d2505e0e16
rodrigo-barboza <rodrigoleandro.adm@gmail.com>
2024-10-26 16:03:46 -0300

    adiciona o provider do websocket no layout do app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Header } from "@/components/Header";
-import { MobileNav } from "@/components/MobileNav";
+import { WebSocketProvider } from "@/context/WebsocketProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>
-        <main>{children}</main>
-      </body>
+      <WebSocketProvider>
+        <body className={inter.className}>
+          <main>{children}</main>
+        </body>
+      </WebSocketProvider>
     </html>
   );
 }
